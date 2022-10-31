220136235b3b99517da021499829ef47fabd39f5
unknown <abhisheksandilya01@gmail.com>
2022-10-31 19:24:24 +0530

    new saved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     <>
       <div className="home" id="home">
         <main>
-          <h1>AbhiStar</h1>
+          <h1>AbhiShek</h1>
           <p>Solution to all your problems</p>
         </main>
       </div>
@@ -22,9 +22,9 @@ const Home = () => {
 
         <div>
           <p>
-            We are your one and only solution to the tech problems you face
-            every day. We are leading tech company whose aim is to increase the
-            problem solving ability in children.
+            This is the first wesite deployed by me 
+            on internet so kindly see this and give your 
+            valuable feedback
           </p>
         </div>
       </div>
@@ -33,27 +33,18 @@ const Home = () => {
         <div>
           <h1>Who we are?</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            officiis hic illo quos sit commodi, aut magnam odio eligendi? Totam
-            quis officiis fugit sunt dolores nostrum tenetur iusto est odio
-            mollitia. Dolor placeat repellendus officia aspernatur dolorum
-            harum. Dolorem cumque quaerat eius qui deserunt ullam laudantium
-            quia tempore. Dignissimos esse sequi numquam est ipsam nemo,
-            voluptas vel recusandae iste aspernatur qui quasi, commodi, atque
-            ipsa dolores tempore asperiores? Dolores soluta placeat molestiae
-            aperiam, ullam officia fugiat corrupti odio, suscipit ipsum cum vel
-            nemo. Ratione quo tempore fugit quaerat! Facilis beatae impedit
-            similique maxime praesentium sunt unde necessitatibus voluptates
-            ipsa, et, numquam reiciendis eveniet? Aliquid nemo nesciunt placeat,
-            qui fuga quae repellat sunt, non natus, iure ab officia rem ipsa cum
-            sed cumque corporis voluptate quibusdam nostrum quod saepe
+            we are unitians we are unitians we are unitians we are unitians
+            we are unitians we are unitians we are unitians we are unitians
+            we are unitians we are unitians we are unitians we are unitians
+            we are unitians we are unitians we are unitians we are unitians
+            we are unitians we are unitians we are unitians we are unitians 
           </p>
         </div>
       </div>
 
       <div className="home4" id="brands">
         <div>
-          <h1>Brands</h1>
+          <h1>Social</h1>
 
           <article>
             <div
